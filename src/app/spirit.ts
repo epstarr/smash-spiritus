77f34ab79ea8139c0ecda85d1f7e93d4f2739423
Evan Starr <evanstar@buchheit.local>
2019-04-01 13:44:02 -0500

    add sprit class

---
+++ src/app/spirit.ts
@@ -0,0 +1,16 @@
+export class Spirit {
+    id: number;
+    image: string;
+    name: string;
+    type: string;
+    class: number;
+    slot: number;
+    min_power: number;
+    max_power: number;
+    min_attack: number;
+    max_attack: number;
+    min_defense: number;
+    max_defense: number;
+    ability: string;
+    origin: string;
+}
