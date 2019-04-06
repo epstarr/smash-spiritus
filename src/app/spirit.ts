271f6db1bc140432a22eed88cdf759cdc86cf85f
epstarr <evanpstarr@gmail.com>
2019-04-05 21:20:15 -0500

    support for spirit slots

--- src/app/spirit.ts
+++ src/app/spirit.ts
@@ -1,11 +1,11 @@
+//https://www.ssbwiki.com/List_of_spirits_(disambiguation)
 export class Spirit {
     public id: number;
     public name: string;
     public image: string;
     public type: string;
     public rarity: number;
-    // class: number;
-    // slot: number;
+    public slot: number;
     // min_power: number;
     // max_power: number;
     // min_attack: number;
@@ -14,11 +14,12 @@ export class Spirit {
     // max_defense: number;
     // ability: string;
     // origin: string;
-    constructor(id: number,name:string,image:string,type:string,rarity:number) {
+    constructor(id: number,name:string,image:string,type:string,rarity:number,slot:number) {
         this.id = id;
         this.name = name;
         this.image = image;
         this.type = type;
         this.rarity = rarity;
+        this.slot = slot
     }
 }
